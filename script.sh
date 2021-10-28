35e214ad4d20e7dc85bce68bb3ab0b51d9815507
Austin Post <87991258+PostMan56@users.noreply.github.com>
2021-10-28 18:50:18 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ then
   read file 
   modified=${file::-6}
   cairo-compile $file --output ${modified}_compiled.json
-  ./test.sh
+  ./script.sh
 fi
 
 if [ $response = run ]
@@ -23,12 +23,12 @@ then
   if [[ $option = print ]]
   then 
     cairo-run --program=${file}_compiled.json --print_output --print_info --relocate_prints
-    ./test.sh
+    ./script.sh
   elif [[ $option = trace ]]
   then
     cairo-run --program=${file}_compiled.json --tracer
   else
-    ./test.sh
+    ./script.sh
   fi
 fi
 
@@ -37,7 +37,7 @@ then
   echo comp - Is used to compile the file given
   echo run - Is used to run the file with few options MAKE SURE TO NOT ADD .CAIRO OR _COMPILED.JSON
   echo run options - print=--print-output --print_info --relocate_prints trace=--tracer
-  ./test.sh
+  ./script.sh
 fi
 
 
